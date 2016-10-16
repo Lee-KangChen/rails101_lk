ace22f88129d00af2182a2b943dce48bf92cc7d6
Lee-KangChen <nihaifong@gmail.com>
2016-10-16 20:00:24 +0800

    user 可以在 group 頁面 加入 / 退出 此 group

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :groups do
-    resources :posts
+    member do
+      post :join
+      post :quit
   end
   root 'groups#index'
   # The priority is based upon order of creation: first created -> highest priority.
